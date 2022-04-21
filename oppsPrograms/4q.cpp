84b9fb8d8644573d674c7c8e7548578dcdf2a2d2
Gautam Sharma <gautam0508.be21@chitkara.edu.in>
2022-04-21 12:14:46 +0530

    4q.cpp added

---
+++ oppsPrograms/4q.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool prime(int a){
+    bool flag=true;
+    for(int i=2;i*i<=a;i++){
+        if(a%i==0){
+            flag=false;
+            break;
+        }
+    }
+    return flag;
+}
+
+int main(){
+    int a;
+    cout<<"enter number you wanna check :";
+    cin>>a;
+
+    if(prime(a)){
+        cout<<a<<"  is a prime number.";
+    }else{
+        cout<<a<<"  is not a prime number.";
+    }
+
+
+
+
+
+    return 0;
+}
